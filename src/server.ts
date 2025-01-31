b25c8b4b58a503fc6e74a5d4c3697847fe876689
Muhammad_Hammad_Shah <Hammad4455shah5544@gmail.com>
2025-01-31 23:26:26 +0500

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 function welcome(name: string) {
     console.log('Welcome' + name);
+    const username = {
+        name: 'hammad',
+    };
+
+    const u1 = username['name'];
+
+    return u1;
 }
 
 welcome('hammad');
